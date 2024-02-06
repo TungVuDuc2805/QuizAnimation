bace1c76b78c9efa2a683a54cac6d2064ad359a8
Tung Vu <tungvuduc2805@gmail.com>
2024-02-06 21:17:27 +0900

    extract questions to a variable

--- Quiz/QuizApp.swift
+++ Quiz/QuizApp.swift
@@ -9,14 +9,16 @@ import SwiftUI
 
 @main
 struct QuizApp: App {
+    let questions = [
+        Question(image: "sports-car", options: ["Car", "Airplane", "Motorbike", "Bicycle"], answer: "Car"),
+        Question(image: "orange",options: ["Strawberry", "Apple", "Orange", "Carrot"], answer: "Orange"),
+        Question(image: "cow", options: ["Tiger", "Cow", "Dog", "Deer"], answer: "Cow"),
+        Question(image: "burger", options: ["Food", "Fruit", "Animal", "Book"], answer: "Food")
+    ]
+    
     var body: some Scene {
         WindowGroup {
-            ContentView(viewModel: ContentViewModel(questions: [
-                Question(image: "sports-car", options: ["Car", "Airplane", "Motorbike", "Bicycle"], answer: "Car"),
-                Question(image: "orange",options: ["Strawberry", "Apple", "Orange", "Carrot"], answer: "Orange"),
-                Question(image: "cow", options: ["Tiger", "Cow", "Dog", "Deer"], answer: "Cow"),
-                Question(image: "burger", options: ["Food", "Fruit", "Animal", "Book"], answer: "Food")
-            ]))
+            ContentView(viewModel: ContentViewModel(questions: questions))
         }
     }
 }
